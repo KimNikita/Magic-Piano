f0c24460cab7b69fe1b0a8dd033ac6d06db4f157
KimNikita <ghj710@mail.ru>
2021-07-12 22:38:04 +0300

    model+module

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import logging as log
 import os
 import sys
+import cv2
+from screeninfo import get_monitors
 from piano import Piano
 from piano_key import PianoKey
+from hand_tracking import HandDetector
 
 
 def main():
     log.basicConfig(format='[ %(levelname)s ] %(message)s',
                     level=log.INFO, stream=sys.stdout)
 
-# Примерный pipeline (как я представляю):
 
 # инициализация значений
+cap = cv2.VideoCapture(0)
+detector = HandDetector()
+monitor = get_monitors()
+
+# генерация клавиш и пианино
+# piano = Piano(100,100,200,200)
 
 # работа нейросети
-# берем выход сети, смотрим какие пальцы закрыли центры каких клавиш, проигрываем звук,
-# изменяем цвет клавиши в зависимости от происходящего
+while cap.isOpened():
+    success, img = cap.read()
+    img = cv2.resize(img, (int(monitor[0].width/2), int(monitor[0].height/2)),
+                     interpolation=cv2.INTER_AREA)
+    hand_points = detector.findPosition(img, True)
 
-# отрисовка
+    # обрабатываем выход сети
 
+    # отрисовка
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
