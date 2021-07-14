411fdac11e690702d6cf987e5e30de7cc7e3e688
Ершов Алескей <AlexVE36@yandex.ru>
2021-07-14 16:10:58 +0300

    Add files via upload

--- sample_sounds.py
+++ sample_sounds.py
@@ -13,13 +13,20 @@ def main():
     f = PianoKey(0, 0, 100, 100, 'A4', path, 1000, 1000)
     k = PianoKey(0, 0, 100, 100, 'B4', path, 1000, 1000)
 
-    while True:
+    for i in range(3):
+        print('Now play C4')
         a.play_sound()
+        print('Now play D4')
         b.play_sound()
+        print('Now play E4')
         c.play_sound()
+        print('Now play F4')
         d.play_sound()
+        print('Now play G4')
         e.play_sound()
+        print('Now play A4')
         f.play_sound()
+        print('Now play B4')
         k.play_sound()
 
 
