271f9c80e0e841caaf665e45d8f84645d5832c31
Goreev Artem <goreevartoum@gmail.com>
2021-07-21 13:34:24 +0300

    fix2

--- src/main.py
+++ src/main.py
@@ -21,12 +21,14 @@ def main():
                     level=log.INFO, stream=sys.stdout)
     spath = os.path.abspath('') + '\\sounds'
     cap = cv.VideoCapture(0)
-    pgame = Game(cap, spath)
+
     monitor = get_monitors()
     m_width = monitor[0].width
     m_height = monitor[0].height
     while cap.isOpened():
         success, img = cap.read()
+        height, width = img.shape[:2]
+        pgame = Game(height, width, spath)
         img = pgame.render(img)
         img = cv.resize(img, (int(m_width / 1.5), int(m_height / 1.5)),
                         interpolation=cv.INTER_AREA)
